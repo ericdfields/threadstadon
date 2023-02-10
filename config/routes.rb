e3e3338c091bcc715910fc92ec7340738ca4ce97
Eric Brookfield <eric@ericbrookfield.com>
2023-02-10 14:16:51 -0500

    Update routes.rb
    that broke things worse

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  constraints(host: 'threadstadon.io') do
-    get '/:param', to: redirect('https://threadstodon.io/%{param}')
-  end
+#   constraints(host: 'threadstadon.io') do
+#     get '/:param', to: redirect('https://threadstodon.io/%{param}')
+#   end
   
   # Defines the root path route ("/")
   root "statuses#new"
