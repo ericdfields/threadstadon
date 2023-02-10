51a046bbf0457691642f49d8ea43a40fac0c0ee1
Eric Brookfield <eric@ericbrookfield.com>
2023-02-10 14:07:11 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  constraints(host: 'threadstadon.io') do
+    get '/:param', to: redirect('https://threadstodon.io/%{param}')
+  end
+  
   # Defines the root path route ("/")
   root "statuses#new"
 
